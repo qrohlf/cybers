b92589efc9a34655bf61b53c9b37ec18f5ede63f
Quinn Rohlf <qr@qrohlf.com>
2014-11-12 13:43:19 -0800

    Nicer ui, colors!

--- cybers.js
+++ cybers.js
@@ -1,39 +1,47 @@
-var cybers = new Firebase('https://cybers.firebaseio.com/');
-var yesterday = new Date().getTime() - 8640000; // 8640000 ms == 1 day
+function do_the_thing() {
+  var cybers = new Firebase('https://cybers.firebaseio.com/');
+  var yesterday = new Date().getTime() - 8640000; // 8640000 ms == 1 day
 
-function addCyber() {
-  var data = {time: new Date().getTime()};
-  cybers.push(data, function(error) {
-    if (error) alert("nice try bud");
+  function addCyber() {
+    var data = {time: new Date().getTime()};
+    cybers.push(data, function(error) {
+      if (error) alert("nice try bud");
+    });
+    console.log(data);
+  }
+
+  function renderCybers(n) {
+    document.getElementById('cybers').innerHTML = n+' cybers today';
+  }
+
+  function authCallback(authData) {
+    if (authData === null) return;
+    console.log("user logged in!");
+    console.log(authData);
+    window.onclick = addCyber;
+    document.body.className = "logged-in";
+  }
+
+  cybers.orderByChild('time').startAt(yesterday).on('value', function(snapshot) {
+    renderCybers(snapshot.numChildren());
   });
-  console.log(data);
+
+  cybers.onAuth(authCallback);
+
+  function login() {
+    console.log("attempting login");
+    cybers.authWithOAuthRedirect("github", function(authData) {
+
+    });
+  }
+
+  window.logout = function() {
+    console.log("bye");
+    cybers.unauth();
+    document.body.className = "logged-out";
+  };
 }
 
-function renderCybers(n) {
-  document.getElementById('cybers').innerHTML = n+' cybers today';
-}
-
-function authCallback(authData) {
-  if (authData === null) return;
-  console.log("user logged in!");
-  console.log(authData);
-  window.onclick = addCyber;
-}
-
-cybers.orderByChild('time').startAt(yesterday).on('value', function(snapshot) {
-  renderCybers(snapshot.numChildren());
+document.addEventListener('DOMContentLoaded', function(){
+  do_the_thing();
 });
-
-cybers.onAuth(authCallback);
-
-function login() {
-  console.log("attempting login");
-  cybers.authWithOAuthRedirect("github", function(authData) {
-
-  });
-}
-
-function logout() {
-  console.log("bye");
-  cybers.unauth();
-}
